eb34897886b149172a91262b78a3cff127295569
Skylar <sliu77@ucsc.edu>
2019-05-08 19:19:12 -0700

    Just made a code its awsome come and check it out

---
+++ Hmm.js
@@ -0,0 +1,17 @@
+//a test code for the git test
+
+var heart;
+var tears = 0;
+var myself;
+
+if (heart.isEmpty()){
+	tears += 1;
+}
+
+if (heart.hasMeme()){
+	while (Meme.isGreat()){
+		myself.Lmao();
+	}
+}
+
+//Warning: this code cannot be runned.
